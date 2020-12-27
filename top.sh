4b058e600562a53790440e756caa496b15a5042e
Niyazbek <55174577+Niyazbekk@users.noreply.github.com>
2020-12-27 07:45:08 +0600

    Update top.sh

--- top.sh
+++ top.sh
@@ -2,7 +2,6 @@
 
 read W
 
-gawk '$9=="404" || $9=="403"' log.txt | gawk '{print $7}' |  sort -k7 | uniq -c | sort -nr | awk '{ $(NF+1) = "-"; $(NF+1) = $1; $1 = NR "."; $(NF+1) = "-"; $(NF+1) = 100 * $4 /87 "%" ; print }' | head -"$W"
-
+gawk '$9=="404" || $9=="403"' log.txt | gawk '{print $7}' |  sort -k7 | uniq -c | sort -nr | awk '{ $(NF+1) = "-"; $(NF+1) = $1; $1 = NR "."; $(NF+1) = "-" ; printf "%s %.1f%\n" , $0 ,$(NF+1) = 100 * $4 /87 "%"}' | head -"$W"
 
 
